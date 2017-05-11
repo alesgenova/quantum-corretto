291e4aab48d046af29bbcd8060ab61c032cd0601
Alessandro Genova <ales.genova@gmail.com>
2017-05-11 17:35:02 -0400

    we can now capture and store var declarations!

--- codeblocks.py
+++ codeblocks.py
@@ -23,12 +23,13 @@ tree_root = 1
 
 
 class Line(object):
-    
+
     def __init__(self, input_string, fileobj_in):
         self.bare = ''
         self.code = []
         self.comment = []
         continue_line = True
+        continuing = False
         #self.empty = False
         while continue_line:
 
@@ -48,60 +49,67 @@ class Line(object):
             try:
                 code = re.match(r'^\s*(&)?\s*(.*?)(&)\s*$',code).group(2)
                 input_string = fileobj_in.readline()
+                continuing = True
             except:
-                code = re.match(r'^\s*(&)?\s*(.*?)(&)?\s*$',code).group(2)
-                continue_line = False
+                # account for empty line, we should continue to the next
+                if continuing and len(code.strip()) == 0:
+                    input_string = fileobj_in.readline()
+                    code = ""
+                else:
+                    continuing = False
+                    continue_line = False
+                    code = re.match(r'^\s*(&)?\s*(.*?)(&)?\s*$',code).group(2)
 
             self.bare += code+' '
             self.code.append(code)
             self.comment.append(comment)
         self.empty = len(self.bare) < 2
-        
 
-            
+
+
 class EmptyLine(Line):
-    
+
     def __init__(self):
         self.bare = ''
         self.code = ['']
         self.comment = ['']
-         
+
 
 class Project(object):
-    
+
     def __init__(self, name):
         self.name = name
         self.sources = []
-        
+
     def add_sourcefile(self, filename):
         self.sources.append(Sourcefile(filename))
-        
+
     def scan_sources(self):
         for src in self.sources:
             #print('Scanning', src.name)
             src.scan()
-        
+
     def make_project_tree(self):
         print('PROJECT '+self.name)
         for src in self.sources:
             print('  |')
             src.print_contains()
-        
+
 
 class Codeblock(object):
-    
+
     def __init__(self,name):
         self.name = name.lower()
         self.contains = OrderedDict()
         self.imports = OrderedDict()
-    
+
     def add_import(self, modulename, variables):
         self.imports[modulename], trash = str_to_arg(variables)
         #self.imports.add_module(modulename,variables)
-  
+
     def add_contains(self,obj):
         self.contains[obj.name] = obj
-        
+
     def print_contains(self):
         print('   '*self.tree_level+'---- '+self.__str__())
         #self.print_contains()
@@ -110,13 +118,13 @@ class Codeblock(object):
             print('   '*self.tree_level+'   |')
             #print ('---'*obj.tree_level,obj.typ,obj.name)
             obj.print_contains()
-    
-        
-        
+
+
+
 
 
 class Sourcefile(Codeblock):
-    
+
     def __init__(self,name):
         super().__init__(name)
         self.name = name
@@ -124,24 +132,25 @@ class Sourcefile(Codeblock):
         self.tree_level = tree_root
         self.typ = 'Sourcefile'
         self.lines = []
-    
+
     def __str__(self):
         return 'FILE '+self.name
-        
+
     def add_line(self, string, fileobj):
         self.lines.append(Line(string,fileobj))
-    
+
     def scan(self):
         context = []
         context.append(self)
         with open(self.name,'r') as src:
+            print(self.name)
             for line in src:
-                
+
                 self.lines.append(Line(line,src))
-                
+
                 # Parse line to update context
                 # (i.e. are we entering a new module/routine/function?)
-                
+
                 if len(self.lines[-1].bare) > 0 :
                     output = line_to_context(self.lines[-1].bare,context)
                     #print(output,context)
@@ -152,7 +161,7 @@ class Sourcefile(Codeblock):
                     if  not output == 0 and not output == 1 :
                         pass
                         #print('WARNING: ',line)
-                        
+
     def print_source(self):
         for line in self.lines:
             npartial = len(line.code)
@@ -162,64 +171,129 @@ class Sourcefile(Codeblock):
                         print( code + ' & ' + line.comment[ipar])
                     else:
                         print( code + line.comment[ipar])
-                
+
             else:
                 print (line.code[0] +' '+ line.comment[0])
-        
+
     def print_bare_source(self):
         for line in self.lines:
             if not line.empty:
                 print(line.bare)
-                
+
 
 
 
 class Procedure(Codeblock):
-    
+
     def __init__(self, name, argstring, parent):
         super().__init__(name)
         self.parent = parent
         self.tree_level = parent.tree_level + 1
         self.args, self.argstring = str_to_arg(argstring)
-        
-        
+
+
 
 class Subroutine(Procedure):
-    
+
     def __init__(self,name,argstring,parent):
         super().__init__(name, argstring, parent)
         self.typ = 'Subroutine'
-    
-    def __str__(self):                
+
+    def __str__(self):
         return 'SUBROUTINE '+self.name+' ('+self.argstring+')'
 
 
 class Function(Procedure):
-    
+
     def __init__(self,name,argstring,parent):
         super().__init__(name, argstring, parent)
         self.typ = 'Function'
-    
-    def __str__(self):                
+
+    def __str__(self):
         return 'FUNCTION '+self.name+' ('+self.argstring+')'
 
-        
+
 class Module(Codeblock):
-    
+
     def __init__(self,name,parent):
         super().__init__(name)
         self.parent = parent
         self.tree_level = parent.tree_level + 1
         self.typ = 'Module'
-    
+
     def __str__(self):
         return 'MODULE '+self.name
 
+def parse_declaration(context,mo):
+    type_base = mo.group('type_base').strip() if mo.group('type_base') else ""
+    type_base = type_base if len(type_base.strip()) > 0 else None
+
+    type_extra = mo.group('type_extra').strip() if mo.group('type_extra') else ""
+    type_extra = type_extra if len(type_extra) > 0 else None
+
+    options = mo.group('options').strip() if mo.group('options') else ""
+    options = options if len(options) > 0 else None
+
+    variables = mo.group('vars').strip() if mo.group('vars') else ""
+    variables = variables if len(variables) > 0 else None
+
+    print(type_base, type_extra, options, variables)
+    attributes = _parse_declaration_options(options)
+    var_list = _parse_declaration_variables(variables, type_base, type_extra, attributes)
+    return
+
+def _parse_declaration_options(options):
+    attributes = {"allocatable":None, "dimension":None, "intent":None , "pointer":None, "target":None}
+    if options is None: return attributes
+    leftover = options
+    mo = options_rgx.match(leftover)
+    while mo is not None:
+        option = mo.group('option')
+        sli = mo.group('slice')
+        if option is not None:
+            if option.lower() in ['allocatable', 'pointer', 'target']:
+                attributes[option.lower()] = True
+            elif option.lower() == "dimension":
+                attributes["dimension"] = sli.strip().lower()
+            elif option.lower() == "intent":
+                attributes["intent"] = sli.strip().lower()
+        #print(mo.group(0))
+        leftover = leftover[len(mo.group(0)):]
+        mo = options_rgx.match(leftover)
+    #print(attributes)
+    return attributes
+
+def _parse_declaration_variables(variables, type_base, type_extra, attributes):
+        var_list = []
+        leftover = variables
+        mo = vars_rgx.match(variables)
+        while mo is not None:
+            var_dict = OrderedDict()
+            var_dict['name'] = mo.group('variable')
+            var_dict['type_base'] = type_base
+            var_dict['type_extra'] = type_extra
+            var_dict['allocatable'] = attributes['allocatable']
+            var_dict['dimension'] = attributes['dimension']
+            var_dict['intent'] = attributes['intent']
+            var_dict['pointer'] = attributes['pointer']
+            var_dict['target'] = attributes['target']
+            var_dict['default'] = mo.group('assign')
+            if mo.group('slice') is not None:
+                var_dict['dimension'] = mo.group('slice').strip()
+            var_list.append(var_dict)
+            #print(mo.group(0))
+            leftover = leftover[len(mo.group('subtract')):]
+            mo = vars_rgx.match(leftover)
+        print()
+        for var in var_list:
+            print(var)
+            print()
+        return var_list
 
 def str_to_arg(string):
     if string is None:
         return None, ''
-    args = vars_rgx.findall(string)
+    args = args_rgx.findall(string)
     argstring = ''
     nargs = len(args)
     if nargs == 0 :
@@ -232,24 +306,31 @@ def str_to_arg(string):
                 argstring += arg
         return args, argstring
     #return args, argstring if len(args) > 0 else None, None
-    
-    
+
+
 def line_to_context(line, context):
-    
+
     curr_context = context[-1]
-    
+
     chk = 0
-    
+
     #print(line)
-    
-    # CHECK USE STATEMENTS 
+
+    # CHECK USE STATEMENTS
     mo = use_rgx.match(line)
     if mo is not None:
         #chk += 1
         curr_context.add_import(mo.group(1), mo.group(2))
         return chk
-           
-    
+
+    # CHECK DECLARATION STATEMENTS
+    mo = declaration_rgx.match(line)
+    if mo is not None:
+        #chk += 1
+        parse_declaration(curr_context,mo)
+        #curr_context.add_import(mo.group(1), mo.group(2))
+        return chk
+
     # CHECK NEW SUBROUTINE
     mo = subroutine_rgx.match(line)
     if mo is not None:
@@ -257,8 +338,8 @@ def line_to_context(line, context):
         context.append(Subroutine(mo.group(1).lower(), mo.group(2),curr_context))
         curr_context.add_contains(context[-1])
         return chk
-        
-    
+
+
     # CHECK END SUBROUTINE
     mo = endsubroutine_rgx.match(line)
     if mo is not None:
@@ -275,7 +356,7 @@ def line_to_context(line, context):
         #print(context[-1])
         context.pop()
         return chk
-    
+
     # CHECK NEW FUNCTION
     mo = function_rgx.match(line)
     if mo is not None:
@@ -283,7 +364,7 @@ def line_to_context(line, context):
         context.append(Function(mo.group(1).lower(), mo.group(2),curr_context))
         curr_context.add_contains(context[-1])
         return chk
-    
+
     # CHECK END FUNCTION
     mo = endfunction_rgx.match(line)
     if mo is not None:
@@ -291,8 +372,8 @@ def line_to_context(line, context):
         #print(context[-1])
         context.pop()
         return chk
-    
-    
+
+
     # CHECK NEW MODULE
     mo = module_rgx.match(line)
     if mo is not None:
@@ -300,16 +381,16 @@ def line_to_context(line, context):
         context.append(Module(mo.group(1).lower(),curr_context))
         curr_context.add_contains(context[-1])
         return chk
-        
+
     # CHECK END MODULE
     mo = endmodule_rgx.match(line)
     if mo is not None:
         chk -= 1
-        
+
         #print(context[-1])
         context.pop()
         return chk
-    
+
     return chk
 
 
@@ -322,4 +403,3 @@ if __name__ == "__main__":
         project.add_sourcefile(fil)
     project.scan_sources()
     project.make_project_tree()
-    
--- f90_regex.py
+++ f90_regex.py
@@ -21,7 +21,7 @@ import re
 
 use_rgx = re.compile(r'^\s*USE\s+(\w+)\s*(?:,\s*ONLY\s*:\s*)?(.*)?',re.IGNORECASE)
 #useonly_rgx = re.compile(r'USE\s+(\w+)(\s*,\s*ONLY\s*:\s*)+',re.IGNORECASE)
-vars_rgx = re.compile(r'(\w+)',re.IGNORECASE)
+args_rgx = re.compile(r'(\w+)',re.IGNORECASE)
 module_rgx = re.compile(r'^\s*module\s+(?!procedure\b)(\w+)',re.IGNORECASE)
 endmodule_rgx = re.compile(r'^\s*end\s*module(?:\s+(\w+))?',re.IGNORECASE)
 subroutine_rgx = re.compile(r'^\s*subroutine\s+(\w+)\s*(?:\((.*)\))?',re.IGNORECASE)
@@ -32,5 +32,5 @@ function_rgx = re.compile(r'^\s*(?:(?:real|complex|integer|character|logical).*\
 endfunction_rgx = re.compile(r'^\s*end\s*function(?:\s+(\w+))?',re.IGNORECASE)
 
 declaration_rgx = re.compile(r'^\s*(?P<type_base>real|complex|integer|character|logical|type)\s*(?:\((?P<type_extra>.*?)\))?\s*,?(?P<options>.*)::\s*(?P<vars>.+)$',re.IGNORECASE)
-options_rgx = re.compile(r'^\s*(?:(?P<option>\w+)(?:\((?P<slice>.*)?\))?\s*,?\s*)',re.IGNORECASE)
-vars_rgx = re.compile(r'^(?P<subtract>,?\s*(?P<option>\w+)\s*(?:\((?P<slice>.*?)?\))?\s*(?:=\s*(?P<assign>.*?))?)(,.*)?$',re.IGNORECASE)
+options_rgx = re.compile(r'^\s*(?:(?P<option>\w+)\s*(?:\((?P<slice>.*)?\))?\s*,?\s*)',re.IGNORECASE)
+vars_rgx = re.compile(r'^(?P<subtract>,?\s*(?P<variable>\w+)\s*(?:\((?P<slice>.*?)?\))?\s*(?:=\s*(?P<assign>.*?))?)(,.*)?$',re.IGNORECASE)
